abdb22236bceeb95108f95306233b4a89ae61260
RUI LU <a10115307@gmail.com>
2019-01-13 12:19:57 +0800

    iterate hashmap

--- restaurant.rb
+++ restaurant.rb
@@ -1,4 +1,5 @@
 restaurant_menu = { "Ramen" => 3, "Dal Makhani" => 4, "Coffee" => 2 }
 restaurant_menu.each do | item, price |
-      puts "#{item}: $#{price}"
+    restaurant_menu[item] = price*1.1
+      puts "#{item}: $#{restaurant_menu[item]}"
 end
